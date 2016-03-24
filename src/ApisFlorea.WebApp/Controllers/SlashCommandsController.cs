8f65e2eb810c85e87a94c56b58496cb2fc64985f
Takaaki Suzuki <anonymous-geek@live.jp>
2016-03-24 15:23:38 +0900

    add /translate command for slack

---
+++ src/ApisFlorea.WebApp/Controllers/SlashCommandsController.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using ApisFlorea.Models.Translation;
+using ApisFlorea.WebApp.Models.Slack;
+using Microsoft.AspNet.Mvc;
+
+
+
+namespace ApisFlorea.WebApp.Controllers
+{
+    /// <summary>
+    /// Slack SlashCommands に関する要求を処理します。
+    /// </summary>
+    public class SlashCommandsController : Controller
+    {
+        #region API
+        /// <summary>
+        /// 指定した言語に文章を翻訳します。
+        /// </summary>
+        /// <param name="request">リクエスト</param>
+        /// <returns>結果</returns>
+        [HttpPost]
+        public async Task<IActionResult> Translate([FromForm] SlashCommandsRequest request)
+        {
+            //--- エラーチェック
+            if (!this.ModelState.IsValid)
+                return this.HttpBadRequest();
+
+            //--- コマンドチェック
+            if (request.Command != "/translate")
+                return this.HttpBadRequest();
+
+            //--- コマンド引数解析
+            const int MinLength = 2;
+            var commands = request.Text.Split(' ');
+            if (commands.Length < MinLength)
+                return this.HttpBadRequest();
+            
+            var to = Language.ByShortName[commands[0]];
+            if (to == null)
+                return this.HttpBadRequest();
+
+            var text = string.Join(" ", commands.Skip(1));
+            if (string.IsNullOrWhiteSpace(text))
+                return this.HttpBadRequest();
+
+            //--- 結果
+            var result = await new BingTranslator().TranslateAsync(null, to, text);
+            return this.Json(new
+            {
+                //response_type = "ephemeral",
+                response_type = "in_channel",
+                text = result?.After ?? "翻訳に失敗しました...",
+            });
+        }
+        #endregion
+    }
+}
---
+++ src/ApisFlorea.WebApp/Models/Slack/SlashCommandsRequest.cs
@@ -0,0 +1,86 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+
+
+namespace ApisFlorea.WebApp.Models.Slack
+{
+    /// <summary>
+    /// SlashCommandsからのリクエストを表します。
+    /// </summary>
+    public class SlashCommandsRequest
+    {
+        #region プロパティ
+        /// <summary>
+        /// トークンを取得または設定します。
+        /// </summary>
+        [Required]
+        [JsonProperty("token")]
+        public string Token { get; set; }
+
+
+        /// <summary>
+        /// チームIDを取得または設定します。
+        /// </summary>
+        [JsonProperty("team_id")]
+        public string TeamId { get; set; }
+
+
+        /// <summary>
+        /// チームドメインを取得または設定します。
+        /// </summary>
+        [JsonProperty("team_domain")]
+        public string TeamDomain { get; set; }
+
+
+        /// <summary>
+        /// チャンネルIDを取得または設定します。
+        /// </summary>
+        [JsonProperty("channel_id")]
+        public string ChannelId { get; set; }
+
+
+        /// <summary>
+        /// チャンネル名を取得または設定します。
+        /// </summary>
+        [JsonProperty("channel_name")]
+        public string ChannelName { get; set; }
+
+
+        /// <summary>
+        /// ユーザーIDを取得または設定します。
+        /// </summary>
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+
+
+        /// <summary>
+        /// ユーザー名を取得または設定します。
+        /// </summary>
+        [JsonProperty("user_name")]
+        public string UserName { get; set; }
+
+
+        /// <summary>
+        /// コマンドを取得または設定します。
+        /// </summary>
+        [Required]
+        [JsonProperty("command")]
+        public string Command { get; set; }
+
+
+        /// <summary>
+        /// テキストを取得または設定します。
+        /// </summary>
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+
+        /// <summary>
+        /// 応答URLを取得または設定します。
+        /// </summary>
+        [JsonProperty("response_url")]
+        public string ResponseUrl { get; set; }
+        #endregion
+    }
+}
